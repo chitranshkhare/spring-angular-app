4fb79589b457d9019b015dd0fdb26d5ba489155a
Chitransh Khare <ckchitransh.khare89@gmail.com>
2018-06-06 22:51:04 +0530

    updated to angular 6

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-	constructor(private http: HttpClient) {
-		
-	}
-	title = 'app';
-
-	ngOnInit(): void {
-		this.http.get("http://localhost:8080/go").subscribe(data => {
-			console.log(data);
-		})
-	}
+export class AppComponent {
+  title = 'app';
 }
-
-
-//https://api.github.com/users/seeschweiler
